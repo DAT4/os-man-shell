f9804dda5dddefda255c26e24a10aa6c52ad4927
Martin <mail@mama.sh>
2020-10-04 15:17:26 +0200

    simplicity

--- experimental.py
+++ experimental.py
@@ -4,29 +4,26 @@ STDIN,STDOUT,STDERR,CHILD = 0,1,2,0
 
 def command(cmd):
     try: execvp(cmd[0].strip(), cmd)
-    except OSError: print('Command not found.')
-    finally: _exit(0)
+    except OSError: print('Command not found.'); _exit(0)
 
 def copy(_close, _duplicate, fd=STDOUT):
     close(_close)
     dup2(_duplicate, fd)
 
-def my_fork(child_stuff, parrent_stuff=lambda:None):
+def my_fork(child, parrent=lambda:None):
     pid = fork()
-    if pid > CHILD: wait(); parrent_stuff()
-    elif pid == CHILD: child_stuff()
+    if pid > CHILD: wait(); parrent()
+    elif pid == CHILD: child()
     else: _exit(0)
 
-def my_pipe(cmd, outer_reading=None, outer_writing=None):
-    def child_stuff():
-        copy(writing,reading,STDIN)
-        if outer_reading is not None: copy(outer_reading, outer_writing)
+def my_pipe(cmd, outer_r=None, outer_w=None):
+    def parrent():
+        if outer_r is not None: copy(outer_r, outer_w)
+        copy(w,r,STDIN)
         command(cmd[len(cmd)-1])
-    def parrent_stuff():
-        if len(cmd) > 2: my_pipe(cmd[:-1], reading, writing)
-        else: copy(reading,writing); command(cmd[0])
-    reading, writing = pipe()
-    my_fork(parrent_stuff,child_stuff)
+    def child(): my_pipe(cmd[:-1], r, w) if len(cmd) > 2 else copy(r,w); command(cmd[0])
+    r, w = pipe()
+    my_fork(child,parrent)
 
 def process(cmd):
     if 'cd' == cmd[0]: chdir(cmd[1])
